f14d3b7a698db1c6b36e8add0c5a3516cc42b1d8
MD-Kayesur <rmdkayesur@gmail.com>
2026-06-21 12:13:37 +0600

    scaffold SignUpScreen component structure

---
+++ src/components/signup-screen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import { ThemedText } from './themed-text';
+
+export function SignUpScreen() {
+  return (
+    <View>
+      <ThemedText>Sign Up Screen Scaffold</ThemedText>
+    </View>
+  );
+}
